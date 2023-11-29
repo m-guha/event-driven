ef78bda4e65544475ac730cf8573ec55e28b0d8d
Jeff Hale <jeffmshale@gmail.com>
2023-11-29 16:53:21 -0500

    add flow and deployment creation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # A repo showing how to create an event-driven workflow with Prefect
+
+We'll use a webhook to get data when a GitHub issue is filed in this repo. The event's information will be passed into a Prefect flow through an automation.
---
+++ flow.py
@@ -0,0 +1,14 @@
+from prefect import flow
+
+
+@flow(log_prints=True)
+def print_issue(issue_number: int, issue_text: str, user_login_name: str) -> None:
+    print(f"Issue #{issue_number} from {user_login_name} reads: {issue_text}")
+    print("Now go do other things with this information (summarize, respond, etc)")
+
+
+if __name__ == "__main__":
+    print_issue.from_source(
+        source="https://github.com/PrefectHQ/GitHub-Issue-Autoresponder.git",
+        entrypoint="flow.py:print_issue",
+    ).deploy(name="gh-issue-deploy", work_pool_name="prefect-managed")
